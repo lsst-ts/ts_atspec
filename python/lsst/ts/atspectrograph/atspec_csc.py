620c23b63c62b9c75c59fb599838d78044b45bfb
Tiago Ribeiro <tiago.astro@gmail.com>
2019-02-15 13:31:10 -0700

    Implement most of the spectrograph CSC. Still pending some important methods and logic though.

--- python/lsst/ts/atspectrograph/atspec_csc.py
+++ python/lsst/ts/atspectrograph/atspec_csc.py
@@ -1,13 +1,23 @@
+import asyncio
+import traceback
+import time
+
 import SALPY_ATSpectrograph
 
-from lsst.ts.salobj import base_csc
+from lsst.ts import salobj
 
 from .model import Model
+from .mock_controller import MockSpectrographController
 
 __all__ = ['CSC']
 
+HEALTH_LOOP_DIED = -300
+LS_ERROR = -301
+FW_ERROR = -302
+GW_ERROR = -303
 
-class CSC(base_csc.BaseCsc):
+
+class CSC(salobj.BaseCsc):
     """
     Commandable SAL Component (CSC) for the Auxiliary Telescope Spectrograph.
     """
@@ -25,7 +35,118 @@ class CSC(base_csc.BaseCsc):
         self.evt_settingVersions.set_put(recommendedSettingsVersion=self.model.recommended_settings,
                                          recommendedSettingsLabels=self.model.settings_labels)
 
-    def do_changeDisperser(self, id_data):
+        self.want_connection = False
+        self._health_loop = None
+
+    def begin_start(self, id_data):
+        """Begin do_start; called before state changes.
+
+        This method call setup on the model, passing the selected setting.
+
+        Parameters
+        ----------
+        id_data : `CommandIdData`
+            Command ID and data
+        """
+        self.model.setup(id_data.data.settingsToApply)
+        self.want_connection = True
+
+    def end_enable(self, id_data):
+        """
+
+        Parameters
+        ----------
+        id_data : `CommandIdData`
+            Command ID and data
+
+        """
+        self._health_loop = asyncio.ensure_future(self.health_monitor_loop())
+
+    async def do_enable(self, id_data):
+        """Transition from `State.DISABLED` to `State.ENABLED`.
+
+        Parameters
+        ----------
+        id_data : `CommandIdData`
+            Command ID and data
+        """
+        # start connection with the controller
+        await self.model.connect()
+        self.want_connection = False
+
+        # initialize the elements
+        await self.home_fw()  # self.model.init_fw(self.want_connection)
+        await self.home_gs()  # self.model.init_gs(self.want_connection)
+        await self.home_gw()  # self.model.init_gw(self.want_connection)
+
+        self._do_change_state(id_data, "enable", [salobj.State.DISABLED], salobj.State.ENABLED)
+
+    async def health_monitor_loop(self):
+        """A coroutine to monitor the state of the hardware."""
+
+        while True:
+            try:
+                ls_state = await self.model.query_gs_status(self.want_connection)
+                fw_state = await self.model.query_fw_status(self.want_connection)
+                gw_state = await self.model.query_gw_status(self.want_connection)
+
+                if self.want_connection:
+                    self.want_connection = False
+
+                # Make sure none of the sub-components are in fault. Go to fault state if so.
+                if ls_state[2] != SALPY_ATSpectrograph.ATSpectrograph_shared_Error_None:
+                    self.fault()
+                    self.log.error(f"Linear stage in error: {ls_state}")
+                    self.evt_errorCode.set_put(errorCode=LS_ERROR,
+                                               errorReport=f"Linear stage in error: {ls_state}")
+                    break
+                elif fw_state[2] != SALPY_ATSpectrograph.ATSpectrograph_shared_Error_None:
+                    self.fault()
+                    self.log.error(f"Filter wheel in error: {fw_state}")
+                    self.evt_errorCode.set_put(errorCode=FW_ERROR,
+                                               errorReport=f"Filter wheel  in error: {fw_state}")
+                    break
+                elif gw_state[2] != SALPY_ATSpectrograph.ATSpectrograph_shared_Error_None:
+                    self.fault()
+                    self.log.error(f"Grating wheel in error: {gw_state}")
+                    self.evt_errorCode.set_put(errorCode=GW_ERROR,
+                                               errorReport=f"Grating wheel in error: {gw_state}")
+                    break
+
+                # # Publish state of each component
+                # if ls_pstate is None or ls_pstate[0] != ls_state[0]:
+                #     self.evt_lsState.set_put(lsState=ls_state[0])
+                #
+                # if fw_pstate is None or fw_pstate[0] != fw_pstate[0]:
+                #     self.evt_fwState.set_put(fwState=fw_state[0])
+                #
+                # if gw_pstate is None or gw_pstate[0] != gw_state[0]:
+                #     self.evt_gwState.set_put(gwState=gw_state[0])
+                #
+                # # Publish position of each component
+                # # FIXME: Need to validate that *_state[1] can be converted to float and int
+                # if ls_pstate is None or ls_pstate[1] != ls_state[1]:
+                #     self.evt_reportedLinearStagePosition.set_put(reportedLinearStagePosition=float(ls_state[1]))
+                #
+                # if fw_pstate is None or fw_pstate[1] != fw_state[1]:
+                #     self.evt_reportedFilterPosition.set_put(reportedFilterPosition=int(fw_state[1]))
+                #
+                # if gw_pstate is None or gw_pstate[1] != gw_state[1]:
+                #     self.evt_reportedDisperserPosition.set_put(reportedDisperserPosition=int(gw_state[1]))
+                #
+                # ls_pstate = ls_state
+                # fw_pstate = fw_state
+                # gw_pstate = gw_state
+
+                await asyncio.sleep(salobj.base_csc.HEARTBEAT_INTERVAL)
+            except Exception as e:
+                self.fault()
+                self.log.exception(e)
+                self.evt_errorCode.set_put(errorCode=HEALTH_LOOP_DIED,
+                                           errorReport="Health loop died for some unspecified reason.",
+                                           traceback=traceback.format_exc())
+
+    async def do_changeDisperser(self, id_data):
         """Change the disperser element.
 
         Parameters
@@ -36,7 +157,14 @@ class CSC(base_csc.BaseCsc):
         """
         self.assert_enabled("changeDisperser")
 
-    def do_changeFilter(self, id_data):
+        await self.move_element(query="query_gw_status",
+                                move="move_gw",
+                                position=id_data.distanceFromHome,
+                                report="reportedDisperserPosition",
+                                inposition="disperserInPosition",
+                                report_state="gwState")
+
+    async def do_changeFilter(self, id_data):
         """Change filter.
 
         Parameters
@@ -47,7 +175,14 @@ class CSC(base_csc.BaseCsc):
         """
         self.assert_enabled("changeFilter")
 
-    def do_homeLinearStage(self, id_data):
+        await self.move_element(query="query_fw_status",
+                                move="move_fw",
+                                position=id_data.distanceFromHome,
+                                report="reportedFilterPosition",
+                                inposition="filterInPosition",
+                                report_state="fwState")
+
+    async def do_homeLinearStage(self, id_data):
         """Home linear stage.
 
         Parameters
@@ -58,7 +193,9 @@ class CSC(base_csc.BaseCsc):
         """
         self.assert_enabled("homeLinearStage")
 
-    def do_moveLinearStage(self, id_data):
+        await self.home_gs()
+
+    async def do_moveLinearStage(self, id_data):
         """Move linear stage.
 
         Parameters
@@ -69,7 +206,18 @@ class CSC(base_csc.BaseCsc):
         """
         self.assert_enabled("moveLinearStage")
 
-    def do_stopAllAxes(self, id_data):
+        # FIXME: Linear stage has a problem and should not be operated at the moment
+        if True:
+            return
+
+        await self.move_element(query="query_ls_status",
+                                move="move_ls",
+                                position=id_data.distanceFromHome,
+                                report="reportedLinearStagePosition",
+                                inposition="linearStageInPosition",
+                                report_state="lsState")
+
+    async def do_stopAllAxes(self, id_data):
         """Stop all axes.
 
         Parameters
@@ -79,3 +227,125 @@ class CSC(base_csc.BaseCsc):
 
         """
         self.assert_enabled("stopAllAxes")
+
+        await self.model.stop_all_motion(self.want_connection)
+
+        # TODO: Report new state and position
+
+    async def implement_simulation_mode(self, simulation_mode):
+        """Implement going into or out of simulation mode.
+
+        Parameters
+        ----------
+        simulation_mode : `int`
+            Requested simulation mode; 0 for normal operation.
+
+        Raises
+        ------
+        ExpectedError
+            If ``simulation_mode`` is not a supported value.
+
+        Notes
+        -----
+        Subclasses should override this method to implement simulation
+        mode. The implementation should:
+
+        * Check the value of ``simulation_mode`` and raise
+          `ExpectedError` if not supported.
+        * If ``simulation_mode`` is 0 then go out of simulation mode.
+        * If ``simulation_mode`` is nonzero then enter the requested
+          simulation mode.
+
+        Do not check the current summary state, nor set the
+        ``simulation_mode`` property nor report the new mode.
+        All of that is handled `do_setSimulationMode`.
+        """
+        if simulation_mode not in (0, 1):
+            raise salobj.ExpectedError(
+                f"Simulation_mode={simulation_mode} must be 0 or 1")
+
+        if self.simulation_mode == simulation_mode:
+            return
+
+        await self.disconnect()
+        await self.stop_mock_ctrl()
+        if simulation_mode == 1:
+            self.mock_ctrl = MockSpectrographController(port=self.port)
+            await asyncio.wait_for(self.mock_ctrl.start(), timeout=2)
+        if self.want_connection:
+            await self.connect()
+
+    async def move_element(self, query, move, position, report, inposition, report_state):
+        """A utility function to wrap the steps for moving the filter wheel, grating
+        wheel and linear stage.
+
+        Parameters
+        ----------
+        query : str
+            Name of the method that queries the status of the element. Must be one of the
+            three options:
+                - query_gw_status
+                - query_fw_status
+                - query_ls_status
+
+        move : str
+            Name of the method that moves the element. Must be one of the
+            three options:
+                - move_gw
+                - move_fw
+                - move_ls
+
+        position : int or float
+            Position to move the wheel or the linear stage. Limits are checked by
+            the controller and an exception is raised if out of range.
+
+        report : str
+            Name of the method responsible for reporting the position. Must be one of the
+            three options:
+                - reportedDisperserPosition
+                - reportedFilterPosition
+                - reportedLinearStagePosition
+
+        inposition : str
+            Name of the method responsible for reporting that element is in position. Must be
+            one of the three options:
+                - disperserInPosition
+                - filterInPosition
+                - linearStageInPosition
+
+        report_state : str
+            Name of the method responsible for reporting the state of the element. Must be
+            one of the three options:
+                - gwState
+                - fwState
+                - lsState
+        """
+
+        p_state = await getattr(self.model, query)(self.want_connection)
+
+        # Send command to the controller. Limit is checked by model.
+        if p_state[0] == SALPY_ATSpectrograph.ATSpectrograph_shared_Status_Stationary:
+            await getattr(self.model, move)(position)
+            getattr(self, f"evt_{report}").set_put(position=p_state[1])
+            getattr(self, f"evt_{inposition}").set_put(inPosition=False)
+        else:
+            getattr(self, f"evt_{report_state}").set_put(state=p_state[0])
+            raise RuntimeError(f"Cannot change position. Current state is {p_state}")
+
+        # Need to wait for command to complete
+        start_time = time.time()
+        while True:
+            state = await getattr(self.model, query)(self.want_connection)
+
+            if p_state[0] != state[0]:
+                getattr(self, f"evt_{report_state}").set_put(state=state[0])
+                p_state = state
+
+            if (state[0] == SALPY_ATSpectrograph.ATSpectrograph_shared_Status_Stationary and
+                    state[1] == position):
+                getattr(self, f"evt_{report}").set_put(position=state[1])
+                getattr(self, f"evt_{inposition}").set_put(inPosition=True)
+                break
+            elif time.time()-start_time > self.model.move_timeout:
+                raise TimeoutError(f"Change position timed out trying to move to "
+                                   f"position {position}.")
--- python/lsst/ts/atspectrograph/model.py
+++ python/lsst/ts/atspectrograph/model.py
@@ -3,6 +3,8 @@ import os
 import yaml
 import asyncio
 
+import SALPY_ATSpectrograph
+
 __all__ = ['Model']
 
 _LOCAL_HOST = "127.0.0.1"
@@ -16,15 +18,15 @@ _limit_decode = {"-": -1,
 class FilterWheelStatus:
     """Class to store possible filter wheel status and error codes."""
 
-    status = {"I": "Initializing/Homing",
-              "M": "Moving",
-              "S": "Stationary/Not moving",
-              "X": "Not at a filter position"}
+    status = {"I": SALPY_ATSpectrograph.ATSpectrograph_shared_Status_Homing,
+              "M": SALPY_ATSpectrograph.ATSpectrograph_shared_Status_Moving,
+              "S": SALPY_ATSpectrograph.ATSpectrograph_shared_Status_Stationary,
+              "X": SALPY_ATSpectrograph.ATSpectrograph_shared_Status_NotInPosition}
 
-    error = {"N": "None",
-             "B": "busy",
-             "I": "not initialized",
-             "T": "move timed out"}
+    error = {"N": SALPY_ATSpectrograph.ATSpectrograph_shared_Error_None,
+             "B": SALPY_ATSpectrograph.ATSpectrograph_shared_Error_Busy,
+             "I": SALPY_ATSpectrograph.ATSpectrograph_shared_Error_NotInitialized,
+             "T": SALPY_ATSpectrograph.ATSpectrograph_shared_Error_MoveTimeout}
 
     def parse_status(self, status):
         """Parse status string.
@@ -100,6 +102,8 @@ class Model:
         self.connection_timeout = 10.
         self.read_timeout = 10.
 
+        self.move_timeout = 60.
+
         self.connect_task = None
         self.reader = None
         self.writer = None
@@ -164,26 +168,32 @@ class Model:
         self.host = self.config['setting'][setting].get('host', _LOCAL_HOST)
         self.port = self.config['setting'][setting].get('port', _DEFAULT_PORT)
 
-    async def stop_all_motion(self):
+    async def stop_all_motion(self, want_connection=False):
         """Send command to stop all motions.
 
         Returns
         -------
         ret_val : str
             Response from controller.
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
 
         """
-        ret_val = await self.run_command("!XXX\r\n")
+        ret_val = await self.run_command("!XXX\r\n", want_connection=want_connection)
 
         return self.check_return(ret_val)
 
-    async def load_program_configuration_from(self, filename):
+    async def load_program_configuration_from(self, filename, want_connection=False):
         """
 
         Parameters
         ----------
         filename : str
             Name of file to load configuration from.
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
 
         Returns
         -------
@@ -195,11 +205,11 @@ class Model:
         RuntimeError
 
         """
-        ret_val = await self.run_command(f"!LDC {filename}\r\n")
+        ret_val = await self.run_command(f"!LDC {filename}\r\n", want_connection=want_connection)
 
         return self.check_return(ret_val)
 
-    async def query_fw_status(self):
+    async def query_fw_status(self, want_connection=False):
         """Query status of the filter wheel.
 
         status : str
@@ -214,16 +224,22 @@ class Model:
             I - not initialized,
             T - move timed out
 
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
         Raises
         ------
         RuntimeError
 
         """
-        ret_val = await self.run_command("?FWS\r\n")
+        ret_val = await self.run_command("?FWS\r\n", want_connection=want_connection)
 
         return FilterWheelStatus().parse_status(self.check_return(ret_val))
 
-    async def query_gw_status(self):
+    async def query_gw_status(self, want_connection=False):
         """Query status of the Grating Wheel.
 
         status : str
@@ -238,16 +254,22 @@ class Model:
             I - not initialized,
             T - move timed out
 
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
         Raises
         ------
         RuntimeError
 
         """
-        ret_val = await self.run_command("?GRS\r\n")
+        ret_val = await self.run_command("?GRS\r\n", want_connection=want_connection)
 
         return GratingWheelStatus().parse_status(self.check_return(ret_val))
 
-    async def query_gs_status(self):
+    async def query_gs_status(self, want_connection=False):
         """Query status of the Grating Linear Stage.
 
         status : str
@@ -262,22 +284,34 @@ class Model:
             I - not initialized,
             T - move timed out
 
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
         Raises
         ------
         RuntimeError
 
         """
-        ret_val = await self.run_command("?GRS\r\n")
+        ret_val = await self.run_command("?GRS\r\n", want_connection=want_connection)
 
         return GratingStageStatus().parse_status(self.check_return(ret_val))
 
-    async def query_gs_limit_switches(self):
+    async def query_gs_limit_switches(self, want_connection=False):
         """Query grating stage limit switches.
 
         Returned status:
             0 not at a limit,
             + at the positive limit,
             - at the negative limit/home
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
 
 
         Returns
@@ -289,12 +323,18 @@ class Model:
         ------
         RuntimeError
         """
-        ret_val = await self.run_command("?LSL\r\n")
+        ret_val = await self.run_command("?LSL\r\n", want_connection=want_connection)
 
         return _limit_decode[self.check_return(ret_val).split(" ")[1]]
 
-    async def query_gw_step_position(self):
+    async def query_gw_step_position(self, want_connection=False):
         """Query grating wheel step position.
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
 
         Returns
         -------
@@ -305,12 +345,18 @@ class Model:
         position : int
             current motor step position
         """
-        ret_val = await self.run_command(f"?GRP\r\n")
+        ret_val = await self.run_command(f"?GRP\r\n", want_connection=want_connection)
 
         return GratingWheelStepPosition().parse_status(self.check_return(ret_val))
 
-    async def query_fw_step_position(self):
+    async def query_fw_step_position(self, want_connection=False):
         """Query filter wheel step position.
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
 
         Returns
         -------
@@ -321,43 +367,94 @@ class Model:
         position : int
             current motor step position
         """
-        ret_val = await self.run_command(f"?FWP\r\n")
+        ret_val = await self.run_command(f"?FWP\r\n", want_connection=want_connection)
 
         return FilterWheelStepPosition().parse_status(self.check_return(ret_val))
 
-    async def init_fw(self):
-        """Initialize/home filter wheel"""
-        ret_val = await self.run_command("!FWI\r\n")
+    async def init_fw(self, want_connection=False):
+        """Initialize/home filter wheel
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
+        """
+        ret_val = await self.run_command("!FWI\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
-    async def init_gw(self):
-        """initialize/home grating wheel"""
-        ret_val = await self.run_command("!GRI\r\n")
+    async def init_gw(self, want_connection=False):
+        """initialize/home grating wheel.
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+        """
+        ret_val = await self.run_command("!GRI\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
-    async def init_gs(self):
-        """initialize grating stage to negative limit/home."""
-        ret_val = await self.run_command("!LSI\r\n")
+    async def init_gs(self, want_connection=False):
+        """initialize grating stage to negative limit/home.
+
+        Parameters
+        ----------
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+        """
+        ret_val = await self.run_command("!LSI\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
-    async def move_fw(self, pos):
-        """move filter wheel to position # (0-3)"""
+    async def move_fw(self, pos, want_connection=False):
+        """move filter wheel to position # (0-3)
+
+        Parameters
+        ----------
+        pos : int
+            Filter id (0-3).
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+        """
         if pos < 0 or pos > 3:
             raise RuntimeError(f"Out of range (0-3), got {pos}.")
-        ret_val = await self.run_command(f"!FWM{pos}\r\n")
+        ret_val = await self.run_command(f"!FWM{pos}\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
-    async def move_gw(self, pos):
-        """move grating wheel to position # (0-3)"""
+    async def move_gw(self, pos, want_connection=False):
+        """move grating wheel to position # (0-3)
+
+        Parameters
+        ----------
+        pos : int
+            Grating id (0-3)
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
+        """
         if pos < 0 or pos > 3:
             raise RuntimeError(f"Out of range (0-3), got {pos}.")
-        ret_val = await self.run_command(f"!GRM{pos}\r\n")
+        ret_val = await self.run_command(f"!GRM{pos}\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
-    async def move_gs(self, pos):
-        """move grating stage to # (mm from home position)"""
+    async def move_gs(self, pos, want_connection=False):
+        """move grating stage to # (mm from home position)
+
+        Parameters
+        ----------
+        pos : float
+            Position from home (in mm).
+        want_connection : bool
+            Boolean to specify if a connection with the controller is to be opened in case it is
+            closed.
+
+        """
         # TODO: limit check?
-        ret_val = await self.run_command(f"!LSM{pos}\r\n")
+        ret_val = await self.run_command(f"!LSM{pos}\r\n", want_connection=want_connection)
         return self.check_return(ret_val)
 
     async def connect(self):
@@ -416,7 +513,7 @@ class Model:
                 read_bytes = await asyncio.wait_for(self.reader.read(1),
                                                     timeout=self.read_timeout)
                 if read_bytes != b">":
-                    raise RuntimeError("Controller not ready...")
+                    raise RuntimeError(f"Controller not ready: Received '{read_bytes}'...")
             except Exception as e:
                 await self.disconnect()
                 raise e
@@ -424,12 +521,16 @@ class Model:
             self.writer.write(f"{cmd}\n".encode())
             await self.writer.drain()
 
-            try:
-                read_bytes = await asyncio.wait_for(self.reader.readuntil("\r\n".encode()),
+            if cmd.startswith("?"):
+                try:
+                    read_bytes = await asyncio.wait_for(self.reader.readuntil("\r\n".encode()),
+                                                        timeout=self.read_timeout)
+                except Exception as e:
+                    await self.disconnect()
+                    raise e
+            else:
+                read_bytes = await asyncio.wait_for(self.reader.read(1),
                                                     timeout=self.read_timeout)
-            except Exception as e:
-                await self.disconnect()
-                raise e
 
             return read_bytes.decode()
 
